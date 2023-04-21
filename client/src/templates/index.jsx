b2dac32e6c509b4ce646b0915e5e198596cc4872
Leonardo Henrique da Silva <77017309+leonardohenri@users.noreply.github.com>
2023-04-20 22:13:28 -0300

    Update index.jsx

--- client/src/templates/index.jsx
+++ client/src/templates/index.jsx
@@ -10,15 +10,16 @@ export const Home = () =>{
  const [estados, setEstados] = useState(); 
  const [cidade, setCidade] = useState();
  const [disabled, setDisabled] = useState(true);
+ const url = "https://servidor-express.onrender.com/"
 
  
   useEffect(()=>{
-    axios.get("http://localhost:3001/getCards").then((res)=>(setCard(res.data)));
-    axios.get("http://localhost:3001/getEstados").then((res)=>(setEstados(res.data)));
+    axios.get(`${url}getCards`).then((res)=>(setCard(res.data)));
+    axios.get(`${url}getEstados`).then((res)=>(setEstados(res.data)));
     
   },[])
   const handleSubmit=()=>{
-    axios.post("http://localhost:3001/register",{
+    axios.post(`${url}register`,{
     name: values.name,
     idade:values.idade,
     cpf: values.cpf,
@@ -32,11 +33,11 @@ export const Home = () =>{
     cidade:values.cidade
 
   }).then((res)=>{console.log(res)})
-  setTimeout(()=>{axios.get("http://localhost:3001/getCards").then((res)=>(setCard(res.data)))},[5000])
+  setTimeout(()=>{axios.get(`${url}getCards`).then((res)=>(setCard(res.data)))},[5000])
   }
   const getCidades = (value) =>{
     console.log(value)
-    axios.get(`http://localhost:3001/getCidades/${value}`).then((res)=>(setCidade(res.data)))
+    axios.get(`${url}getCidades/${value}`).then((res)=>(setCidade(res.data)))
     setDisabled(false);
   }
   
